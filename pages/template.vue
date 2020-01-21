7057a14e4ee7eb05755e69008b09765ecd47e950
yoshinari nihei <n0802jp2@gmail.com>
2020-01-22 02:06:00 +0900

    add template page

---
+++ pages/template.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+<style lang="scss">
+body {
+  background: gray;
+  img {
+    background: red;
+  }
+}
+</style>
